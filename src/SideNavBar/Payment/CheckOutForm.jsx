7927befc691c397bbf93b7d656eebe91ab5ca6c0
Mehedi <tanjildewan10@gmail.com>
2023-12-27 22:45:23 +0600

    build psayment method

--- src/SideNavBar/Payment/CheckOutForm.jsx
+++ src/SideNavBar/Payment/CheckOutForm.jsx
@@ -1,10 +1,25 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import useAxiosSecure from '../../hooks/useAxiousSecure';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const CheckOutForm = ({price}) => {
   const stripe = useStripe();
   const elements = useElements();
+  const {user} = useContext(AuthContext)
   const [error,setError] = useState()
+  const axiosSecure = useAxiosSecure()
+  const [clientSecret, setClientSecret] = useState('')
+  const [processing,setProcessing] = useState(false)
+
+  useEffect(() => {
+    axiosSecure.post('/create-payment-intent',{price})
+    .then(res=>{
+      console.log(res.data)
+      setClientSecret(res.data.clientSecret)
+    })    
+  }, []);
 
   const handleSubmit = async(event) =>{
     event.preventDefault();
@@ -32,6 +47,39 @@ const CheckOutForm = ({price}) => {
       console.log('[PaymentMethod]', paymentMethod);
       setError(' ')
     }
+    setProcessing(true)
+
+    const {paymentIntent, error:paymentError} = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email,
+            name: user?.displayName,
+          },
+        },
+      },
+    );
+
+    if(paymentError){
+        console.log(paymentError)
+    }
+    else{
+      console.log('Successfully payment: ',paymentIntent)
+    }
+    setProcessing(false)
+
+    if(paymentIntent.status === "succeeded"){
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "payment success",
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+
   }
 
   return (
@@ -52,7 +100,7 @@ const CheckOutForm = ({price}) => {
         },
       }}
     />
-    <button type="submit" disabled={!stripe}>
+    <button className='btn btn-neutral' type="submit" disabled={!stripe || !clientSecret || processing}>
       Pay
     </button>
     <p className='text-red-500'>{error}</p>
